02284aa19f976bd4df3292dad7f1f30948680832
Umar Faruq Adamu <Umaralfaruq02@gmail.com>
2019-08-24 13:22:53 +0000

    added login id

--- app/src/main/java/com/example/gona/RegisterActivity.java
+++ app/src/main/java/com/example/gona/RegisterActivity.java
@@ -13,10 +13,17 @@ public class RegisterActivity extends AppCompatActivity {
     Button btnRegister;
     EditText ufirstname, ulastname, uothername, upassword, ucpassword, uemail;
     TextInputLayout userFirstNameWrapper, userLastNameWrapper, userOtherNameWrapper, userEmailWrapper,
-            userPasswordWrapper, userConfpasswordWrapper;
+                    userPasswordWrapper, userConfpasswordWrapper;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        ufirstname=findViewById(R.id.userFirstName);
+        ulastname=findViewById(R.id.userLastName);
+        uothername=findViewById(R.id.userOtherNames);
+        upassword=findViewById(R.id.userPassword);
+        ucpassword=findViewById(R.id.userConfirmPassword);
+        uemail=findViewById(R.id.user)
     }
 }
