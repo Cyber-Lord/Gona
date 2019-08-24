9fb5d84a0e02cf46943e4c2a8b0873ca8b2aa465
Umar Faruq Adamu <Umaralfaruq02@gmail.com>
2019-08-24 12:28:58 +0000

    Added id's to activity register

--- app/src/main/java/com/example/gona/RegisterActivity.java
+++ app/src/main/java/com/example/gona/RegisterActivity.java
@@ -3,9 +3,17 @@ package com.example.gona;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.android.material.textfield.TextInputLayout;
 
 public class RegisterActivity extends AppCompatActivity {
 
+    Button btnRegister;
+    EditText ufirstname, ulastname, uothername, upassword, ucpassword, uemail;
+    TextInputLayout userFirstNameWrapper, userLastNameWrapper, userOtherNameWrapper, userEmailWrapper,
+            userPasswordWrapper, userConfpasswordWrapper;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
